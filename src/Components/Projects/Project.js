f70d6f90f2c0a686f237c1d5f3278c8e35755ad3
gauravsinhaweb <sinhagaurav.me@gmail.com>
2021-05-09 22:12:34 +0530

    commit

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -14,15 +14,22 @@ function Project() {
         <>
           {" "}
           <div className="project-list">
-            <div
-              data-aos-offset="300"
-              data-aos-easing="ease-in-sine"
-              data-aos-duration="500"
-              data-aos="fade-right"
-              className="card"
+            {" "}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://moviezoid.netlify.app/"
             >
-              <img src={test} alt="" />
-            </div>
+              <div
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="500"
+                data-aos="fade-right"
+                className="card"
+              >
+                <img src={test} alt="" />
+              </div>{" "}
+            </a>
             <div className="card-disable" data-aos="fade-up">
               <p>
                 The <strong>MovieZoid</strong> is a website that shows a
@@ -44,26 +51,40 @@ function Project() {
                 Authentication Service.
               </p>
             </div>{" "}
-            <div
-              data-aos-offset="300"
-              data-aos-easing="ease-in-sine"
-              data-aos-duration="500"
-              data-aos="fade-left"
-              className="card"
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://authentication-firebase.netlify.app/"
             >
-              <img src={test} alt="" />
-            </div>
+              {" "}
+              <div
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="500"
+                data-aos="fade-left"
+                className="card"
+              >
+                <img src={test} alt="" />
+              </div>{" "}
+            </a>
           </div>{" "}
           <div className="project-list">
-            <div
-              data-aos-offset="300"
-              data-aos-easing="ease-in-sine"
-              data-aos-duration="500"
-              data-aos="fade-right"
-              className="card"
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://gauravsinhaweb-twitterclone.netlify.app/"
             >
-              <img src={test} alt="" />
-            </div>
+              {" "}
+              <div
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="500"
+                data-aos="fade-right"
+                className="card"
+              >
+                <img src={test} alt="" />
+              </div>{" "}
+            </a>
             <div className="card-disable" data-aos="fade-up">
               <p>
                 <strong>Twitter clone</strong> is an app where I tried to clone
@@ -85,15 +106,22 @@ function Project() {
                 connected by Google Firebase.
               </p>
             </div>{" "}
-            <div
-              data-aos-offset="300"
-              data-aos-easing="ease-in-sine"
-              data-aos-duration="500"
-              data-aos="fade-left"
-              className="card"
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://gauravsinhaweb.github.io/TodoList-App/"
             >
-              <img src={test} alt="" />
-            </div>
+              {" "}
+              <div
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="500"
+                data-aos="fade-left"
+                className="card"
+              >
+                <img src={test} alt="" />
+              </div>
+            </a>
           </div>{" "}
         </>
       ) : (
@@ -101,15 +129,23 @@ function Project() {
           {" "}
           <div>
             <div className="project-list">
-              <div
-                data-aos-offset="300"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="500"
-                data-aos="fade-right"
-                className="card"
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://moviezoid.netlify.app/"
               >
-                <img src={test} alt="" />
-              </div>
+                {" "}
+                <div
+                  data-aos-offset="300"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="500"
+                  data-aos="fade-right"
+                  className="card"
+                >
+                  <img src={test} alt="" />
+                </div>
+              </a>
+
               <div className="card-disable" data-aos="fade-up">
                 <p>
                   <strong>MovieZoid</strong>
@@ -121,6 +157,60 @@ function Project() {
             {" "}
             <div className="project-list">
               {" "}
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://authentication-firebase.netlify.app/"
+              >
+                {" "}
+                <div
+                  data-aos-offset="300"
+                  data-aos-easing="ease-in-sine"
+                  data-aos-duration="500"
+                  data-aos="fade-left"
+                  className="card"
+                >
+                  <img src={test} alt="" />
+                </div>
+              </a>
+              <div className="card-disable" data-aos="fade-up">
+                <p>
+                  <strong>Form Authentication</strong>
+                </p>
+              </div>
+            </div>{" "}
+          </div>
+          <div className="project-list">
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://gauravsinhaweb-twitterclone.netlify.app/"
+            >
+              {" "}
+              <div
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+                data-aos-duration="500"
+                data-aos="fade-right"
+                className="card"
+              >
+                <img src={test} alt="" />
+              </div>
+            </a>
+            <div className="card-disable" data-aos="fade-up">
+              <p>
+                <strong>Twitter clone</strong>
+              </p>
+            </div>
+          </div>
+          <div className="project-list">
+            {" "}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://gauravsinhaweb.github.io/TodoList-App/"
+            >
+              {" "}
               <div
                 data-aos-offset="300"
                 data-aos-easing="ease-in-sine"
@@ -130,40 +220,7 @@ function Project() {
               >
                 <img src={test} alt="" />
               </div>
-              <div className="card-disable" data-aos="fade-up">
-                <p>
-                  <strong>Form Authentication</strong>
-                </p>
-              </div>
-            </div>{" "}
-          </div>
-          <div className="project-list">
-            <div
-              data-aos-offset="300"
-              data-aos-easing="ease-in-sine"
-              data-aos-duration="500"
-              data-aos="fade-right"
-              className="card"
-            >
-              <img src={test} alt="" />
-            </div>
-            <div className="card-disable" data-aos="fade-up">
-              <p>
-                <strong>Twitter clone</strong>
-              </p>
-            </div>
-          </div>
-          <div className="project-list">
-            {" "}
-            <div
-              data-aos-offset="300"
-              data-aos-easing="ease-in-sine"
-              data-aos-duration="500"
-              data-aos="fade-left"
-              className="card"
-            >
-              <img src={test} alt="" />
-            </div>
+            </a>
             <div className="card-disable" data-aos="fade-up">
               <p>
                 <strong>TodoList-App</strong>
