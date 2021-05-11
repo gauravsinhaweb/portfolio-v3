bf4dd62f55294b647f7c6aa7e8ece2646d83e878
gauravsinhaweb <sinhagaaurav37@gmail.com>
2021-05-12 02:36:35 +0530

    commit

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -47,8 +47,7 @@ function Project() {
               <p>
                 <strong>Form Authentication</strong> aims to make building
                 secure authentication systems easy, while improving the sign-in
-                and onboarding experience for end users using Google Firebase
-                Authentication Service.
+                and onboarding experience for end users.
               </p>
             </div>{" "}
             <a
@@ -90,8 +89,7 @@ function Project() {
                 <strong>Twitter clone</strong> is an app where I tried to clone
                 the Twitter app. The reason behind making this app because I was
                 curious to know about how the scroll works in three individual
-                columns. Also, I will connect this app to firebase so that the
-                Tweets will be accessible through any device.
+                columns.
               </p>
             </div>
           </div>
@@ -102,8 +100,7 @@ function Project() {
                 <strong>TodoList-App</strong> is a CRUD App where you can
                 Create, Read, Update and Delete the data. It is a simple
                 note-taking platform, where you can write all the important
-                to-dos and delete the tasks after they're done. The Database is
-                connected by Google Firebase.
+                to-dos and delete the tasks after they're done.
               </p>
             </div>{" "}
             <a
@@ -127,35 +124,40 @@ function Project() {
       ) : (
         <>
           {" "}
-          <div>
-            <div className="project-list">
+          <div className="card-mini">
+            <div
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              data-aos="fade-right"
+            >
+              {" "}
               <a
                 rel="noopener noreferrer"
                 target="_blank"
                 href="https://moviezoid.netlify.app/"
               >
                 {" "}
-                <div
-                  data-aos-offset="300"
-                  data-aos-easing="ease-in-sine"
-                  data-aos-duration="500"
-                  data-aos="fade-right"
-                  className="card"
-                >
-                  <img src={test} alt="" />
-                </div>
+                <img src={test} alt="" />
               </a>
-
-              <div className="card-disable" data-aos="fade-up">
-                <p>
-                  <strong>MovieZoid</strong>
-                </p>
-              </div>
             </div>
-          </div>
-          <div>
-            {" "}
-            <div className="project-list">
+            <div data-aos="fade-up">
+              <p>
+                {" "}
+                The <strong>MovieZoid</strong> is a website that shows a
+                collection of Movies and TV shows and related Information,
+                reviews, Dates of Release, Star Ratings, overview, etc. It is
+                Made by using tmdb API. This App is responsive for Mobile and
+                Desktop Devices. It supports static rendering {`&`} pagination
+                as well.
+              </p>{" "}
+            </div>
+            <div
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              data-aos="fade-left"
+            >
               {" "}
               <a
                 rel="noopener noreferrer"
@@ -163,69 +165,64 @@ function Project() {
                 href="https://authentication-firebase.netlify.app/"
               >
                 {" "}
-                <div
-                  data-aos-offset="300"
-                  data-aos-easing="ease-in-sine"
-                  data-aos-duration="500"
-                  data-aos="fade-left"
-                  className="card"
-                >
-                  <img src={test} alt="" />
-                </div>
+                <img src={test} alt="" />
               </a>
-              <div className="card-disable" data-aos="fade-up">
-                <p>
-                  <strong>Form Authentication</strong>
-                </p>
-              </div>
+            </div>
+            <div data-aos="fade-up">
+              <p>
+                <strong>Form Authentication</strong> aims to make building
+                secure authentication systems easy, while improving the sign-in
+                and onboarding experience for end users.
+              </p>
+            </div>
+            <div
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              data-aos="fade-right"
+            >
+              {" "}
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://gauravsinhaweb-twitterclone.netlify.app/"
+              >
+                {" "}
+                <img src={test} alt="" />
+              </a>
+            </div>
+            <div data-aos="fade-up">
+              <p>
+                <strong>Twitter clone</strong> is an app where I tried to clone
+                the Twitter app. The reason behind making this app because I was
+                curious to know about how the scroll works in three individual
+                columns.
+              </p>
             </div>{" "}
-          </div>
-          <div className="project-list">
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://gauravsinhaweb-twitterclone.netlify.app/"
+            <div
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              data-aos="fade-left"
             >
               {" "}
-              <div
-                data-aos-offset="300"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="500"
-                data-aos="fade-right"
-                className="card"
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://gauravsinhaweb.github.io/TodoList-App/"
               >
+                {" "}
                 <img src={test} alt="" />
-              </div>
-            </a>
-            <div className="card-disable" data-aos="fade-up">
-              <p>
-                <strong>Twitter clone</strong>
-              </p>
+              </a>
             </div>
-          </div>
-          <div className="project-list">
-            {" "}
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://gauravsinhaweb.github.io/TodoList-App/"
-            >
-              {" "}
-              <div
-                data-aos-offset="300"
-                data-aos-easing="ease-in-sine"
-                data-aos-duration="500"
-                data-aos="fade-left"
-                className="card"
-              >
-                <img src={test} alt="" />
-              </div>
-            </a>
-            <div className="card-disable" data-aos="fade-up">
+            <div data-aos="fade-up">
               <p>
-                <strong>TodoList-App</strong>
+                <strong>TodoList-App</strong> is a CRUD App where you can
+                Create, Read, Update and Delete the data. It is a simple
+                note-taking platform, where you can write all the important
+                to-dos and delete the tasks after they're done.
               </p>
-            </div>
+            </div>{" "}
           </div>
         </>
       )}
