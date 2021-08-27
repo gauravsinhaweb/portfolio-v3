c0faef78984056759c2533126c63cc386dfe85d6
gauravsinhaweb <sinhagaaurav37@gmail.com>
2021-08-28 01:08:47 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./pages/App";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "aos/dist/aos.css"; // You can also use <link> for styles
